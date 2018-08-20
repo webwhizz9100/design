0c0d11634a4021c46905f864c8fcb76f6cc58c00
Tiago Pires <tiago.pires@localsearch.com.au>
2018-08-20 15:53:09 +1000

    Add the readIdeas() function

--- javascript/crud.js
+++ javascript/crud.js
@@ -1,2 +1,21 @@
+
 // save our Ideas container
 var ideasUL = document.getElementById('ideas');
+
+// render the ideas into the container
+function readIdeas(ideas, container){
+	
+	// render from sratch on every call
+	container.innerHTML = '';
+
+	for(var i = 0; i < ideas.length; i++){
+		var idea = ideas[i];
+		var ideasTemplate =  `
+			<li class="Idea">
+				<input class="Idea_edit" type="text"value="${idea.title.rendered}"/>
+				<button class="Idea_delete" >Delete</button>
+			</li>
+		`;
+		container.innerHTML += ideasTemplate;
+	}
+}
