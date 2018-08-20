59c56a40b2d3a02be463b899380964ca0410c20a
Tiago Pires <tiago.pires@localsearch.com.au>
2018-08-21 00:08:54 +1000

    Add createIdea() and call it when create input changes content

--- javascript/crud.js
+++ javascript/crud.js
@@ -1,22 +1,57 @@
 
-// save our Ideas container
+/**
+ * DOM elements
+ */
+
 var ideasUL = document.getElementById('ideas');
 
-// make a request for Ideas
-var ideas = jQuery.ajax(
-	{
-    	url: 'https://www.getdone.pw/wp-json/wp/v2/idea',
-    	method: 'GET',
-	}
-).done(function(response){
+/**
+ * Event Listeners
+ */
 
-	// render Ideas
-	readIdeas(response, ideasUL);
+document.addEventListener("DOMContentLoaded", function(e) { 
+  
+	readIdeas();
 
 });
 
+document.addEventListener('change', function(e){
+
+	var changed = e.target;
+
+	if(changed.matches('#idea_create')){
+
+		createIdea(changed.value);
+
+		changed.value = '';
+	}
+
+});
+
+
+/** 
+ * Functions
+ */
+
+function readIdeas(){
+
+	jQuery.ajax(
+		{
+	    	url: 'https://www.getdone.pw/wp-json/wp/v2/idea',
+	    	method: 'GET',
+		}
+	).done(function(response){
+
+		// render Ideas
+		renderIdeas(response, ideasUL);
+
+	});
+
+}
+
+
 // render the ideas into the container
-function readIdeas(ideas, container){
+function renderIdeas(ideas, container){
 	
 	// render from sratch on every call
 	container.innerHTML = '';
@@ -26,9 +61,26 @@ function readIdeas(ideas, container){
 		var ideasTemplate =  `
 			<li class="Idea">
 				<input class="Idea_edit" type="text"value="${idea.title.rendered}"/>
-				<button class="Idea_delete" >Delete</button>
+				<button class="Idea_delete">Delete</button>
 			</li>
 		`;
 		container.innerHTML += ideasTemplate;
 	}
+}
+
+
+function createIdea(title){
+	jQuery.ajax({
+		url: 'https://www.getdone.pw/wp-json/wp/v2/idea', 
+		method: 'POST',
+		beforeSend: function(xhr){
+			xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('auth_token'));
+		},
+		data: {
+			title: title,
+			status: 'publish'
+		}
+	}).done(function(response){
+		readIdeas();
+	});
 }
