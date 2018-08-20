87cd2dea41a50cbeebc0b5f6576594d85d73407a
Tiago Pires <tiago.pires@localsearch.com.au>
2018-08-20 23:16:31 +1000

    Cache DOM elements of our form and login process

--- javascript/authenticate.js
+++ javascript/authenticate.js
@@ -1,3 +1,20 @@
+/**
+ * DOM elements
+ */
+
+var form = document.getElementById('login_form');
+var password = document.getElementById('password');
+var username = document.getElementById('username');
+var loginButton = document.getElementById('login_button');
+var logoutButton = document.getElementById('logout_button');
+var createTaskEl = document.getElementById('Task_create');
+var body = document.getElementsByTagName('body')[0];
+
+
+/**
+ * Functions
+ */
+
 function getToken(username, password){
 
 	jQuery.ajax({
